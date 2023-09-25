caac6e3bb22e4990dcca2d3a97be5a2fda5e3539
Renato <renato.f.fabri@gmail.com>
2023-09-25 20:10:37 -0300

    add popstate event handler to BrowserRouter

--- src/BrowserRouter.jsx
+++ src/BrowserRouter.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 const BrowserContext = createContext();
 const routerState = {};
 
 const BrowserRouter = ({ children }) => {
   const [currentURL, setCurrentURL] = useState(new URL(window.location.href));
+
+  useEffect(() => {
+    function handlePopStateEvent(e) {
+      console.log(e);
+    }
+    window.addEventListener("popstate", handlePopStateEvent);
+
+    return () => window.removeEventListener("popstate", handlePopStateEvent);
+  }, []);
+
   return (
     <BrowserContext.Provider value={{ routerState, currentURL, setCurrentURL }}>
       {children}
